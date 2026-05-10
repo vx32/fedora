1bce904edbdf6889c23f5a560a88fc33e9d3944b
vx32 <x64wall@proton.me>
2026-05-11 01:23:53 +0800

    copr2

--- autosub/conf.mk
+++ autosub/conf.mk
@@ -1,4 +1,4 @@
 autosub:
         dnf -y install rpm-build rpmdevtools libcurl-devel libcurl
-        spectool -g -R autosub/autosub.spec
-        rpmbuild -bs --define "_srcrpmdir $(outdir)" autosub/autosub.spec
+        spectool -g -R $(spec)/autosub.spec
+        rpmbuild -bs --define "_srcrpmdir $(outdir)" $(spec)/autosub.spec
