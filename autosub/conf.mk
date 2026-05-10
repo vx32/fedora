f4d9ad495d47d7a1b8fda0ee2ce2750fac7068b0
vx32 <x64wall@proton.me>
2026-05-11 01:26:58 +0800

    fix: makefile space -> tab

--- autosub/conf.mk
+++ autosub/conf.mk
@@ -1,4 +1,4 @@
 autosub:
-        dnf -y install rpm-build rpmdevtools libcurl-devel libcurl
-        spectool -g -R $(spec)/autosub.spec
-        rpmbuild -bs --define "_srcrpmdir $(outdir)" $(spec)/autosub.spec
+	dnf -y install rpm-build rpmdevtools libcurl-devel libcurl
+	spectool -g -R $(spec)/autosub.spec
+	rpmbuild -bs --define "_srcrpmdir $(outdir)" $(spec)/autosub.spec
